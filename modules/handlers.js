9040be252a88bf7b9dcf7f017847dfaee6fcdcb7
Amanda Dzierżanowska <dzierzanowska.amanda@gmail.com>
2018-08-13 12:56:16 +0200

    Init

---
+++ modules/handlers.js
@@ -0,0 +1,38 @@
+var fs = require('fs');
+var formidable = require('formidable');
+
+
+exports.welcome = function(request, response) {
+    console.log("Rozpoczynam obsługę żądania welcome.");
+    fs.readFile('templates/start.html', function(err, html) {
+        response.writeHead(200, {"Conten-Type": "text/htm; charset=utf-8"});
+        response.write(html);
+        response.end();
+    });
+}
+
+exports.upload = function(request, response) {
+  console.log("Rozpoczynam obsługę żądania upload.");
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(error, fields, files) {
+    fs.renameSync(files.upload.path, "test.jpg");
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image:<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
+}
+
+exports.show = function(request, response) {
+    fs.readFile("test.jpg", "binary", function(error, file) {
+        response.writeHead(200, {"Content-Type": "image/jpg"});
+        response.write(file, "binary");
+        response.end();
+    });
+}
+
+exports.error = function(request, response) {
+    console.log("Nie wiem co robić.");
+    response.write("404 :(");
+    response.end();
+}
